f2d8562ae93dd0a36b2d205827a44e66d046e0f6
RalitsaZh <81959937+RalitsaZh@users.noreply.github.com>
2021-06-13 22:52:53 +0300

    Delete src directory

--- src/redux/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import currentUserReducer from "../AppService/CurrentUser.reducer"
-const rootReducer = combineReducers({
-        currentUser:currentUserReducer,
-});
-
-export default rootReducer;
