aa31a71b561d631a2155e01743d8fcbab3e2f07e
RalitsaZh <81959937+RalitsaZh@users.noreply.github.com>
2021-06-13 22:48:19 +0300

    Delete src/utils directory

--- src/utils/debounce.js
+++
@@ -1,9 +0,0 @@
-function debounce(func, time) {
-    let timerId;
-    return function () {
-      clearTimeout(timerId);
-      timerId = setTimeout(func, time);
-    };
-  }
-
-export  {debounce};
